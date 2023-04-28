2a7b7cd2163ecaa689210c8404f223b7859ea628
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-28 20:21:42 -0300

    Exercício usando o temporiazador

--- Aula8_pt2/Aula8_pt2.ino
+++ Aula8_pt2/Aula8_pt2.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
-
+#define LED PD7
+int contador = 0;
+ISR (TIMER0_OVF_vect) {
+  if (contador > 9) {    // 1 segundo 
+    PORTD ^= (1 << LED);  // inverte o estado do LED
+    contador = 0;
+  }
+  contador ++;
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+int main()
+{
+  DDRD |= (1 << LED);
+  
+  TCCR0A = 0;                          // modo normal
+  TCCR0B = (1 << CS02) | (1 << CS00);  // 1024
+  TIMSK0|= (1 << TOIE0);               // habilita interrupção no fim da contagem
+  sei();                               // habilita interrupção global 
+  
+  while (1) {
+   
+  }
 }
