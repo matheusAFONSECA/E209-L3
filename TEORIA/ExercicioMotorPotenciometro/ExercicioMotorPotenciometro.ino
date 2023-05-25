3644844545da66e1ef95ca740ab5fd5f9a6e64cb
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-05-25 18:57:50 -0300

    Exercicio OK

--- TEORIA/ExercicioMotorPotenciometro/ExercicioMotorPotenciometro.ino
+++ TEORIA/ExercicioMotorPotenciometro/ExercicioMotorPotenciometro.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#define motor PD6
 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int main(){
+  DDRD |= (1 << motor);     // definindo o motor como saida
+  // conversor AD:
+  ADMUX |= (1 << REFS0);
+  ADCSRA |= (1 << ADEN);    // liga o ADC
+  ADCSRA |= (1 << ADSC);    // inicializo a conversão
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);   // prescaler = 128
+  
+  //PWM
+  TCCR0A |= (1 << WGM00) |(1 << WGM01);     // MODO PWM RÁPIDO
+  TCCR0A |= (1 << COM0A1) |(1 << COM0A0);   // LIGA O PINA PWM NA COMPARAÇÃO DO CONTADOR COM OCR
+  TCCR0B |= (1 << CS01);      // PRESCALER = 8
+  OCR0A = 200;   // INICIALIZA O VALOR DE OCR
+  int conv;
+  
+  Serial.begin(9600);   // inicia a comunicação serial
 
+  while(1){
+    while(ADCSRA & (1 << ADSC));   // aguarda o fim da conversão
+    Serial.println(ADC);
+    conv = map(ADC, 0, 1023, 0, 225);   // coloca a leitura do POT entre 0 e 255
+    OCR0A = conv;
+
+    ADCSRA |= (1 << ADSC);      // inicia uma nova conversão
+  }
+  
 }
